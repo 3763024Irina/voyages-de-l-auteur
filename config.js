373c5f27db2bf4c8114e709d36df833b1f7ee6e3
Irina Sakovich <96925002+3763024Irina@users.noreply.github.com>
2025-08-22 15:45:05 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+// /config.js
+window.APP_CONFIG = {
+  email: '3763024@gmail.com',
+  whatsapp: '33759644813', // номер без "+"
+  needguide: 'https://needguide.ru/view_guide.php?user_id=22306'
+};
